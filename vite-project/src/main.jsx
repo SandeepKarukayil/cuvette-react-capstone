1e4abdb6bf70d7a9896c19feb6a2c2884efe5bff
SandeepKarukayil <115607146+SandeepKarukayil@users.noreply.github.com>
2024-06-18 21:03:29 +0530

    first commit

---
+++ vite-project/src/main.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Info from "./pages/Info.jsx";
+import Browse from "./pages/Browse.jsx";
+import Selection from "./pages/MovieSelection.jsx";
+import Notfound from "./pages/404.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<Home/>}
+        />
+        <Route
+          path="/info"
+          element={<Info/>}
+        />
+        <Route
+          path="/browse"
+          element={<Browse/>}
+        />
+        <Route
+          path="/selection"
+          element={<Selection/>}
+        />
+        <Route
+          path="/*"
+          element={<Notfound/>}
+        />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
---
+++ vite-project/src/pages/404.jsx
@@ -0,0 +1,12 @@
+import { useNavigate } from "react-router-dom";
+
+export default function Notfound() {
+    const navigate = useNavigate()
+  return (
+    <div style={{textAlign:"center"}}>
+      <h1>404</h1>
+      <h2>Page not found</h2>
+      <button onClick={()=>navigate('/')}>Back to Home</button>
+    </div>
+  );
+}
